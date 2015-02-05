aae9322021698f22b0f137920e2c962d5beb1cad
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-02-05 18:01:38 -0500

    Formatting fixing.

--- vpc.c
+++ vpc.c
@@ -1,12 +1,12 @@
 /*
     vpc is a virtual memory program.
 
-    author:		Dave Smith-Hayes
+    author:     Dave Smith-Hayes
 */
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TOTAL_MEMORY    16000U	/* 16kB */
+#define TOTAL_MEMORY    16000U  /* 16kB */
 
 /* Function prototypes */
 void display_help();
@@ -17,49 +17,49 @@ int  load_file(void *memory, unsigned int max);
 /* Entry point */
 int main(int argc, char *argv[])
 {
-	/* Variables */
-	char memory[TOTAL_MEMORY];
-	char input;
-	int result;
-	
-	/* The menu and operations */
-	fprintf(stdout, "'h' or '?' for help, 'q' to quit.\n");
-	get_input(&input);
-       
-	while(1) {
-		switch(input) {
-			/* load the file */
-			case 'l':
-				if((result = load_file(memory, TOTAL_MEMORY)) > 0)
-					fprintf(stdout, "File loaded.\n");
-				else if(result == -1)
-					fprintf(stdout, "Error loading file.\n");
-				else if(result == -2)
-					fprintf(stdout, "Error reading the file.\n");
-				else if(result == -3)
-					fprintf(stdout, "File is empty.\n");
-				
-				break;
-				
-			/* write the file */    
-			case 'w':
-				
-				break;
-			
-			/* display the help menu */
-			case '?':
-			case 'h':
-				display_help();
-				break;
-		}
-		
-		if(input == 'q')
-			break;
-		
-		get_input(&input);
-	}
-	
-	return 0;
+    /* Variables */
+    char memory[TOTAL_MEMORY];
+    char input;
+    int result;
+    
+    /* The menu and operations */
+    fprintf(stdout, "'h' or '?' for help, 'q' to quit.\n");
+    get_input(&input);
+    
+    while(1) {
+        switch(input) {
+            /* load the file */
+            case 'l':
+                if((result = load_file(memory, TOTAL_MEMORY)) > 0)
+                    fprintf(stdout, "File loaded.\n");
+                else if(result == -1)
+                    fprintf(stdout, "Error loading file.\n");
+                else if(result == -2)
+                    fprintf(stdout, "Error reading the file.\n");
+                else if(result == -3)
+                    fprintf(stdout, "File is empty.\n");
+                
+                break;
+                
+            /* write the file */    
+            case 'w':
+                
+                break;
+            
+            /* display the help menu */
+            case '?':
+            case 'h':
+                display_help();
+                break;
+        }
+        
+        if(input == 'q')
+            break;
+        
+        get_input(&input);
+    }
+    
+    return 0;
 }
 
 /*
@@ -68,16 +68,16 @@ int main(int argc, char *argv[])
 */
 void display_help()
 {
-	fprintf(stdout, "d\tdump memory\n");
-	fprintf(stdout, "g\tgo - run the program\n");
-	fprintf(stdout, "l\tload a file into memory\n");
-	fprintf(stdout, "m\tmemory modify\n");
-	fprintf(stdout, "q\tquit\n");
-	fprintf(stdout, "r\tdisplay registers\n");
-	fprintf(stdout, "t\ttrace - execute one instruction\n");
-	fprintf(stdout, "w\twrite file\n");
-	fprintf(stdout, "z\treset all registers to 0\n");
-	fprintf(stdout, "?, h\tdisplay this menu\n\n");
+    fprintf(stdout, "d\tdump memory\n");
+    fprintf(stdout, "g\tgo - run the program\n");
+    fprintf(stdout, "l\tload a file into memory\n");
+    fprintf(stdout, "m\tmemory modify\n");
+    fprintf(stdout, "q\tquit\n");
+    fprintf(stdout, "r\tdisplay registers\n");
+    fprintf(stdout, "t\ttrace - execute one instruction\n");
+    fprintf(stdout, "w\twrite file\n");
+    fprintf(stdout, "z\treset all registers to 0\n");
+    fprintf(stdout, "?, h\tdisplay this menu\n\n");
 }
 
 /*
@@ -85,8 +85,8 @@ void display_help()
 */
 void get_input(char *in)
 {
-	fprintf(stdout, "> ");
-	*in = (char)fgetc(stdin);
+    fprintf(stdout, "> ");
+    *in = (char)fgetc(stdin);
 }
 
 /*
@@ -99,34 +99,34 @@ void write_file(void *memory) { }
 */
 int load_file(void *memory, unsigned int max)
 {
-	char *file_name;			/* user given */
-	static FILE *f;				/* the actual file */
-	static char *mode = "a+";	/* change this? */
-	unsigned int size;
-	
-	fprintf(stdout, "file: ");
-	fscanf(stdin, "%s", file_name);
-	
-	/* -1 on failure to open the file */
-	if((f = fopen((const char *)file_name, mode)) == NULL)
-		return -1;
-	
-	/* -2 on failure to seek to the end */
-	if(fseek(f, 0, SEEK_END) == 0) 	/* Sucess */
-		size = ftell(f);
-	else
-		return -2;
-	
-	if(size > max) {
-		fprintf(stdout, "File truncated...\n");
-		size = max;
-	}
-	
-	rewind(f);
-	
-	/* -3 means it didn't read shit. */
-	if(fgets(memory, size, f) == NULL)
-		return -3;
-	else
-		return size;
+    char *file_name;            /* user given */
+    static FILE *f;             /* the actual file */
+    static char *mode = "a+";   /* change this? */
+    unsigned int size;
+    
+    fprintf(stdout, "file: ");
+    fscanf(stdin, "%s", file_name);
+    
+    /* -1 on failure to open the file */
+    if((f = fopen((const char *)file_name, mode)) == NULL)
+        return -1;
+    
+    /* -2 on failure to seek to the end */
+    if(fseek(f, 0, SEEK_END) == 0)      /* Sucess */
+        size = ftell(f);
+    else
+        return -2;
+    
+    if(size > max) {
+        fprintf(stdout, "File truncated...\n");
+        size = max;
+    }
+    
+    rewind(f);
+    
+    /* -3 means it didn't read shit. */
+    if(fgets(memory, size, f) == NULL)
+        return -3;
+    else
+        return size;
 }
