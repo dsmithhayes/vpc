8cd1e7e5d3c41dd9dc62bb208bfd7f3f7cedf00e
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-03-13 17:53:57 -0400

    What am I even doing.

--- src/fetch.c
+++ src/fetch.c
@@ -10,8 +10,19 @@
 #include "vpc.h"
 #include "registers.h"
 
+#define BITS_PER_BYTE   8;
+
+
 void
 fetch(void *memory)
 {
+    int i = 0;
+    
     mar = registers[PC];
+    registers[PC] += REG_SIZE;
+
+    do {
+        mbr += mbr << BITS_PER_BYTE;            /* HA! I used it! */
+        mbr = ((char *) memory + (mar + i++));
+    } while (i != REG_SIZE);
 }
