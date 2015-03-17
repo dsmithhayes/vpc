6e765634be9ce65b98927b2e99d56165e9a83074
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-03-16 20:04:51 -0400

    Implementing Go!

---
+++ src/go.c
@@ -0,0 +1,17 @@
+/*
+    Routines here will actually make the program operate.
+
+    file:       go.c
+    author:     Dave Smith-Hayes
+    date:       March 16th, 2015
+*/
+
+#include <stdio.h>
+#include "registers.h"
+
+void
+go(void *memory, struct registers *reg)
+{
+
+    return;
+}
