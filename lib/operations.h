6331684887e0ce358adb54ae169a0454f1852547
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-03-17 14:07:57 -0400

    Starting the execution of instructions.

---
+++ lib/operations.h
@@ -0,0 +1,21 @@
+/*
+    Defines all of the macros and prototypes used for analyzing
+    instructions for the CPU.
+    
+    file:       operations.h
+    author:     Dave Smith-Hayes
+    date:       March 17, 2015
+*/
+
+
+/*************************/
+/* Types of Instructions */
+/*************************/
+
+#define DATA_PROC           /* data processing        */
+#define LOAD_STORE          /* load/store from memory */
+#define IMMEDIATE           /* obvious, I think       */
+#define CON_BR              /* conditional branch     */
+#define UNCON_BR            /* unconditional branch   */
+#define PUSH_PULL           /* obvious                */
+#define STOP                /* the best instruction   */
---
+++ src/execute.c
@@ -0,0 +1,9 @@
+/*
+    Executes the instruction given
+    
+    file:       execute.c
+    author:     Dave Smith-Hayes
+    date:       March 17, 2015
+*/
+
+#define "operations.h"
