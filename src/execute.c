1866d244a13654914a8e06eeffef551b95b9bb18
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-03-26 14:53:32 -0400

    There is a better way to do this. I swear.

--- src/execute.c
+++ src/execute.c
@@ -21,61 +21,25 @@
 void
 execute(uint16_t inst, registers *reg)
 {
-    /**************************/
-    /* Immediate Instructions */
-    /**************************/
+    int i;
 
-    /* Move immediate value into Rd */
-    if(is_inst(MOV_IMM, inst)) {
-        immediate(MOV_IMM, inst, reg);
-        return;
+    for(i = 0; i < TOTAL_INST; i++) {
+        /*
+         * Determine immediate
+         */
+
+        /*
+         * Determine data
+         */
+
+        /*
+         * Determine branch
+         */
+
+        /*
+         * Determine stop
+         */
     }
-
-    /* Compare immediate value to Rd */
-    if(is_inst(CMP_IMM, inst)) {
-        immediate(CMP_IMM, inst, reg);
-        return;
-    }
-
-    /* Add immediate value into Rd */
-    if(is_inst(ADD_IMM, inst)) {
-        immediate(ADD_IMM, inst, reg);
-        return;
-    }
-
-    /* Subtract immediate value from Rd */
-    if(is_inst(SUB_IMM, inst)) {
-        immediate(SUB_IMM, inst, reg);
-        return;
-    }
-
-
-    /*********************/
-    /* Data Instructions */
-    /*********************/
-
-    if(is_inst(AND_DAT, inst)) {
-        data(AND_DAT, inst, reg);
-        return;
-    }
-
-    if(is_inst(EOR_DAT, inst)) {
-        data(EOR_DAT, inst, reg);
-        return;
-    }
-
-    if(is_inst(ORR_DAT, inst)) {
-        data(AND_DAT, inst, reg);
-        return;
-    }    
-
-
-    /**************/
-    /* Stop flags */
-    /**************/
-
-    if(is_inst(STOP_INST, inst))
-        toggle_flg(STOP, (uint32_t *) &(reg->stp_flg));
     
     return;
 }
