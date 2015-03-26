3bb3eaf454a67324680afe58886da39e2578fb7f
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-03-26 10:44:06 -0400

    Same structure as immediate(), but different operations.

--- src/data.c
+++ src/data.c
@@ -10,8 +10,16 @@
 #include "operations.h"
 
 void
-data(uint16_t mask, uint16_t instr, registers *reg)
+data(uint16_t mask, uint16_t inst, registers *reg)
 {
+    uint8_t rd = get_rd(inst);
+    uint8_t rn = get_rn(inst);
 
+    switch(mask) {
+
+    }
+
+    toggle_flg(INST, (uint32_t) &(reg->ir_flag));
+    
     return;
 }
