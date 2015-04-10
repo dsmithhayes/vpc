f934498da11357c6a07b0a792a9baf4b8c8ee440
Dave Smith-Hayes <dsmithhayes@gmail.com>
2015-04-10 14:00:09 -0400

    Pushing and pulling the stack is all that's left (that's a lie but its not in the repo)

---
+++ src/pushpull.c
@@ -0,0 +1,20 @@
+/*
+    file:       pushpull.c
+    author:     Dave Smith-Hayes
+    date;       April 10, 2015
+    
+    The push and pull instruction for the stack. This will store
+    or load register values from memory.
+*/
+
+#include <stdint.h>
+
+#include "registers.h"
+#include "operations.h"
+
+void
+pushpull(void *memory, register *reg)
+{
+    
+    return;
+}
